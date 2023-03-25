952b6a94e0d931cd7f552c9b288c6fcdc6aa75f3
Alex <alex_lanes@hotmail.com>
2023-03-25 16:49:31 -0300

    Input de usuário

---
+++ src/Input.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Input {
+    public static void main( String[] args ) throws Exception {
+        ArrayList< String > lista = new ArrayList<>();
+        Scanner scanner = new Scanner( System.in );
+
+        System.out.println( "Digite alguma coisa, 'sair' para encerrar o loop: " );
+
+        while( true ){
+            String texto = scanner.next();
+            if( "sair".equals(texto) ) 
+                break;
+        }
+            
+        scanner.close();
+        System.out.println( "Você escreveu: " + lista );
+    }
+}
